df93e619eec5fafbb9e839c67783915576e15f8f
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-09-08 16:18:00 -0700

    * Modified backup script for remote server

--- ouraapp/backup_db.sh
+++ ouraapp/backup_db.sh
@@ -5,17 +5,17 @@ export PATH="/Users/jackdescombes/.venvs/flaskbook/bin:/Users/jackdescombes/opt/
 max_files=5
 today=$(date +"%m-%d-%y")
 
-filename="/Users/jackdescombes/db_backups/oura_db_$today.bak"
+filename="/var/lib/ouraapp/backups/uploads/oura_db_$today.bak"
 
-cd /Users/jackdescombes/projects/oura_proj/ouraapp
+cd /srv/jwa
 
 pg_dump oura_db > "$filename"
 
-cd /Users/jackdescombes/db_backups/
+cd /var/lib/ouraapp/backups/uploads/
 
 chmod 777 *
 
-aws s3 sync /Users/jackdescombes/db_backups/ s3://ouradbbackups
+aws s3 sync /var/lib/ouraapp/backups/uploads/ s3://ouradbbackups
 
 file_count=$(aws s3 ls --recursive s3://ouradbbackups | wc -l)
 excess=$(($file_count-$max_files))
@@ -27,4 +27,4 @@ ls -t | tail -$excess | xargs rm
 aws s3 sync . s3://ouradbbackups --delete
 fi
 
-rsync -avp /Users/jackdescombes/db_backups/* root@server.jpdesc.com:/var/lib/ouraapp/backups/uploads/
+rsync -avp root@server.jpdesc.com:/var/lib/ouraapp/backups/uploads/* /Users/jackdescombes/db_backups/
