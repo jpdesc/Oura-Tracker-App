651731fb8f4ef30904d95ddcd4a7def8c46a1267
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-10-17 15:05:21 -0700

    * Hash password in profile.

--- ouraapp/profile/routes.py
+++ ouraapp/profile/routes.py
@@ -5,6 +5,7 @@ from ouraapp.auth.models import User
 from ouraapp.extensions import db
 from ouraapp.auth.forms import RegistrationForm
 from ouraapp.profile import bp
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @bp.route(f'/profile/<int:edit>', methods=['GET', 'POST'])
@@ -13,9 +14,11 @@ def profile(edit):
     user = User.query.filter_by(id=current_user.id).first()
     username = user.username
     name = user.name
-    password = user.password
+    password = user.password_hash
     oura_access_token = user.oura_access_token
     join_date = user.join_date
+    hashed_password = generate_password_hash(registration_form.password1.data,
+                                             "sha256")
     form = RegistrationForm(username=username,
                             password1=password,
                             password2=password,
@@ -25,7 +28,8 @@ def profile(edit):
         user.username = form.username.data
         user.name = form.name.data
         user.email = form.email.data
-        user.password = form.password1.data
+        user.password_hash = generate_password_hash(form.password1.data,
+                                                    "sha256")
         user.oura_access_token = form.oura_access_token.data
         db.session.add(user)
         db.session.commit()
