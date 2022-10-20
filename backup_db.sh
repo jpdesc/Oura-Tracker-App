0a92aadbdc08d0ce42016041c56726afc2368965
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-10-20 13:39:16 -0700

    * edit backup script.

--- backup_db.sh
+++ backup_db.sh
@@ -9,7 +9,7 @@ filename="/var/lib/ouraapp/backups/uploads/oura_db_$today.bak"
 
 cd /srv/jwa
 
-pg_dump -h localhost -U postgres -Fc oura_db > "$filename"
+pg_dump -U postgres -Fc oura_db > "$filename"
 
 cd /var/lib/ouraapp/backups/uploads/
 
