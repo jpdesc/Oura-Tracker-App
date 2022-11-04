4a97caa424f1d931202ab35263b0bc86c81d66a5
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-11-04 14:23:46 -0700

    * Weights functional tests.

---
+++ ouraapp/tests/functional/weights_test.py
@@ -0,0 +1,50 @@
+from . import force_login, login_user
+from ouraapp.auth.helpers import setup_oura_data
+
+
+@force_login(cb=lambda s: login_user(s, 1))
+def test_weights__logged_in(loaded_db_client):
+    response = loaded_db_client.get('/workout/weights/306')
+    assert response.status_code == 200
+
+
+def test_weights__logged_out(loaded_db_client):
+    response = loaded_db_client.get('/workout/weights/306')
+    assert response.status_code == 302
+
+
+@force_login(cb=lambda s: login_user(s, 1))
+def test_edit_weights__logged_in(loaded_db_client):
+    response = loaded_db_client.get('/workout/edit_weights/from_base:yes/306')
+    assert response.status_code == 200
+    response = loaded_db_client.get('/workout/edit_weights/from_base:no/306')
+    assert response.status_code == 200
+
+
+def test_edit_weights__logged_out(loaded_db_client):
+    response = loaded_db_client.get('/workout/edit_weights/from_base:no/306')
+    assert response.status_code == 302
+    response = loaded_db_client.get('/workout/edit_weights/from_base:no/306')
+    assert response.status_code == 302
+
+
+@force_login(cb=lambda s: login_user(s, 1))
+def test_create_template__logged_in(loaded_db_client):
+    response = loaded_db_client.get('/workout/create_template/test/1/306')
+    assert response.status_code == 200
+
+
+def test_create_template__logged_out(loaded_db_client):
+    response = loaded_db_client.get('/workout/create_template/test/1/306')
+    assert response.status_code == 302
+
+
+@force_login(cb=lambda s: login_user(s, 1))
+def test_init_template__logged_in(loaded_db_client):
+    response = loaded_db_client.get('/workout/create_template/test/1/306')
+    assert response.status_code == 200
+
+
+def test_init_template__logged_out(loaded_db_client):
+    response = loaded_db_client.get('/workout/create_template/test/1/306')
+    assert response.status_code == 302
