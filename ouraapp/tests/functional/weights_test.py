9b83181ac79cfa004314e5f6f20e69ff7470e872
Jack DesCombes <jpdesc@alumni.stanford.edu>
2023-01-30 18:39:59 -0800

    Fix tests

--- ouraapp/tests/functional/weights_test.py
+++ ouraapp/tests/functional/weights_test.py
@@ -15,16 +15,16 @@ def test_weights__logged_out(loaded_db_client):
 
 @force_login(cb=lambda s: login_user(s, 1))
 def test_edit_weights__logged_in(loaded_db_client):
-    response = loaded_db_client.get('/workout/edit_weights/from_base:yes/306')
+    response = loaded_db_client.get('/workout/edit_weights/from_base:True/306')
     assert response.status_code == 200
-    response = loaded_db_client.get('/workout/edit_weights/from_base:no/306')
+    response = loaded_db_client.get('/workout/edit_weights/from_base:False/306')
     assert response.status_code == 200
 
 
 def test_edit_weights__logged_out(loaded_db_client):
-    response = loaded_db_client.get('/workout/edit_weights/from_base:no/306')
+    response = loaded_db_client.get('/workout/edit_weights/from_base:True/306')
     assert response.status_code == 302
-    response = loaded_db_client.get('/workout/edit_weights/from_base:no/306')
+    response = loaded_db_client.get('/workout/edit_weights/from_base:False/306')
     assert response.status_code == 302
 
 
